4078897cf0e6f9e0b0a21754c5072e934f7aa475
Albert-oficial <candelariomacalopusiesquen@gmail.com>
2026-08-20 01:24:06 -0500

    ANZY DX VERSION 1.09.2

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ const TU_NUMERO = '51996399291';
 const JID_DUEÑO = `${TU_NUMERO}@s.whatsapp.net`;
 const PUERTO = process.env.PORT || 3000;
 const LIMITE_DIARIO_ESTIMADO = 1400;
-const MAX_TOKENS_RESPUESTA = 500;
+const MAX_TOKENS_RESPUESTA = 1500;
 
 const COMANDO_LLAMADA_IA = '/anzy';
 
